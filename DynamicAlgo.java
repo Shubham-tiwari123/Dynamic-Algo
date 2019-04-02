573316421c0e7d029d066fca137c36c4f652f589
Shubham-tiwari123 <st540362@gmail.com>
2019-04-02 23:35:22 +0530

    Algo

--- DynamicAlgo.java
+++ DynamicAlgo.java
@@ -41,10 +41,44 @@ public class DynamicAlgo {
         System.out.println("list:-"+nextUglyno);
     }
     
+    void goldMineProblem(int gold[][],int m,int n){
+        
+        int goldTable[][] = new int[m][n];
+        for(int col=n-1;col>=0;col--){
+            for(int row=0;row<m;row++){
+                int right = (col == n-1) ? 0 
+                        : goldTable[row][col+1]; 
+                int right_up = (row == 0 || 
+                              col == n-1) ? 0 : 
+                        goldTable[row-1][col+1];
+                 int right_down = (row == m-1 
+                            || col == n-1) ? 0 : 
+                          goldTable[row+1][col+1]; 
+                 goldTable[row][col] = gold[row][col] 
+                 + Math.max(right, Math.max(right_up,  
+                                       right_down)); 
+            }
+        }
+        int res = goldTable[0][0]; 
+          
+        for (int i = 1; i < m; i++) 
+            res = Math.max(res, goldTable[i][0]); 
+              
+        System.out.println("result:-"+res);
+    }
+    
     public static void main(String[] args) {
         DynamicAlgo algo = new DynamicAlgo();
         //algo.fibonacciNumber(3);
-        algo.uglyNumber(150);
+        //algo.uglyNumber(150);
+        
+        int gold[][]= { {1, 3, 1, 5}, 
+                        {2, 2, 4, 1}, 
+                        {5, 0, 2, 3}, 
+                        {0, 6, 1, 2} }; 
+                          
+        int m = 4, n = 4; 
+        algo.goldMineProblem(gold, m, n);
     }
     
 }
